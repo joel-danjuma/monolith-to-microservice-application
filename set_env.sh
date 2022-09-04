e7e2fe0f04dca24cd807b91e26297c7e32d95273
joel danjuma <joeldanjuma@joels-MacBook-Air.local>
2022-09-04 04:50:50 +0100

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagram_postgres
+export POSTGRES_PASSWORD=udagram_postgres
+export POSTGRES_HOST=database-1.cuqh6onb68vx.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-monolith
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
